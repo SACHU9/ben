0f4d8c79b98465b36aee073e69c579977396a1e1
SACHU9 <fenyfelicitas123@gmail.com>
2017-03-06 14:34:02 +0530

    Add files via upload

---
+++ a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int integer;
+printf("\n give the integer");
+scanf("%d",&integer);
+if(integer<o)
+integer=-integer;
+printf("integer=%d",integer);
+}
---
+++ add.c
@@ -0,0 +1,13 @@
+\\addtio
+#include<stdio.h>
+int main()
+{
+int a,b;
+float c;
+a=5;
+b=2;
+c=a+b;
+printf("sum of %d and %d is %.2f\n"a,b,c);
+return 0;
+}
+
